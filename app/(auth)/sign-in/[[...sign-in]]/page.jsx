0648c03ebf65b90aa62520913a9d1dfeaf0f23f1
Kishan <kishansinghthakur27@gmail.com>
2025-09-05 16:17:12 +0530

    improving some minor issues

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,7 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return (<div className='flex items-center justify-center h-screen'>
+  return (<div className='flex items-center justify-center h-screen  bg-gradient-to-br from-gray-900 via-gray-850 to-gray-800'>
     <SignIn />
   </div>)
 }
